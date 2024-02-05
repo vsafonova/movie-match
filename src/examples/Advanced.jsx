bf39f3ec9395a4ef3c7a7c8ded51b417248b6241
vsafonova <viktoriyablak@gmail.com>
2024-02-05 17:15:44 +0100

    Change content

--- src/examples/Advanced.jsx
+++ src/examples/Advanced.jsx
@@ -153,14 +153,14 @@ function Advanced() {
           style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
           onClick={() => swipe("left")}
         >
-          Swipe left!
+          Swipe left
         </button>
 
         <button
           style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
           onClick={() => swipe("right")}
         >
-          Swipe right!
+          Swipe right
         </button>
       </div>
     </div>

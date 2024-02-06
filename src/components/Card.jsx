af2ade37ba5699579dba18293387a84fb053ab60
vsafonova <viktoriyablak@gmail.com>
2024-02-06 17:24:53 +0100

    Working on styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,15 +6,18 @@ export default function Card({ title, image, description, genres }) {
   const imageUrl = "./public/images/" + image;
   return (
     <>
-      <div className="card">
-        <h1>{title}</h1>
-        <img src={imageUrl} alt="Poster image" width={200} height={300}></img>
-        <h4>Description</h4>
-        <p>{description}</p>
-        <div>
-          {genres.map((genre, i) => {
-            return <span key={i}>{genre + " "} </span>;
-          })}
+      <div
+        className="card"
+        style={{ backgroundImage: "url(" + imageUrl + ")" }}
+      >
+        <div className="card-detail">
+          <h3 className="card-title">{title}</h3>
+          <p className="card-description">{description}</p>
+          <div class="card-footer">
+            {genres.map((genre, i) => {
+              return <span class="card-tags" key={i}>{genre + " "} </span>;
+            })}
+          </div>
         </div>
       </div>
     </>

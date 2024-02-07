90d7aa2e4c9ddbf55be9499406afe74fb4f2e3f9
vsafonova <viktoriyablak@gmail.com>
2024-02-07 21:09:01 +0100

    change files according final version of the project

---
+++ src/components/Advanced.jsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect, useContext, useMemo, useRef } from "react";
+import TinderCard from "react-tinder-card";
+import { MatchProviderContext } from "../providers/matchProvider";
+import NameInput from "./NameInput";
+import ResultPage from "./Results";
+import Instructions from "./Instructions";
+import Card from "./Card";
+
+function Advanced() {
+  const {
+    cardData,
+    userData,
+    setUserData,
+    showResult,
+    showNameInput,
+    setShowNameInput,
+    showInstructions,
+  } = useContext(MatchProviderContext);
+
+  const [currentIndex, setCurrentIndex] = useState();
+  const [lastDirection, setLastDirection] = useState();
+
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
+
+  useEffect(() => {
+    updateCurrentIndex(cardData.length - 1);
+    setShowNameInput(false);
+  }, [cardData.length]);
+
+  useEffect(() => {
+    if (currentIndex < 0) {
+      setShowNameInput(true);
+    }
+  }, [currentIndex]);
+
+  const childRefs = useMemo(() => {
+    return Array(cardData.length)
+      .fill(0)
+
+      .map((i) => React.createRef());
+  }, [cardData.length]);
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+
+    currentIndexRef.current = val;
+  };
+
+  const canSwipe = currentIndex >= 0;
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+
+    //logic for collecting data goes here
+
+    if (direction === "right") {
+      let obj = userData;
+
+      obj.push(cardData[index].id);
+
+      setUserData(obj);
+    }
+
+    setLastDirection(direction);
+
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name, idx) => {
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < cardData.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+
+  return (
+    <div className="card-wrapper">
+      <Instructions />
+      {showResult && <ResultPage />}
+      <h1 className="app-title">Movie Match</h1>
+      <NameInput />
+      <div
+        className="card-container"
+        style={{ visibility: showInstructions ? "hidden" : "visible" }}
+      >
+        {cardData.map((entry, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className="swipe"
+            key={entry.title}
+            onSwipe={(dir) => swiped(dir, entry.title, index)}
+            onCardLeftScreen={() => outOfFrame(entry.title, index)}
+          >
+            <div>
+              <Card
+                isCurrent={currentIndex === index ? "current" : ""}
+                title={entry.title}
+                description={entry.description}
+                image={entry.image}
+                genres={entry.genres}
+              />
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+      <div
+        className="swipe-buttons"
+        style={{
+          visibility: currentIndex < 0 ? "hidden" : "visible",
+        }}
+      >
+        <button
+          className="button"
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("left")}
+        >
+          👎
+        </button>
+
+        <button
+          className="button"
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("right")}
+        >
+          👍
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Advanced;
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,36 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { MatchProviderContext } from "../providers/matchProvider";
 import { prepareComment } from "../helpers/PrepareComment";
 import useFetch from "../hooks/useFetch";
 
 export default function ResultPage() {
-  const { cardData, userData, userName, showResult } =
-    useContext(MatchProviderContext);
-
-  let quote = "";
-
+  const { cardData, userData, userName, showResult } = useContext(MatchProviderContext);
   const payload = { userName: userName, userData: userData };
-  const { data, loading, error } = useFetch(
-    "https://arashbesharat.com/wp-json/movie-match/api/v1/submit",
-    "POST",
-    payload
-  );
+  const { data, loading, error } = useFetch("https://arashbesharat.com/wp-json/movie-match/api/v1/submit", "POST", payload);
 
-  console.log("RESULT", data);
+  const quote = prepareComment(userData, cardData, userName);
 
-  quote = prepareComment(userData, cardData, userName);
+  useEffect(() => {
+    console.log("Data:", data);
+    console.log("Error:", error);
+  }, [data, error]);
 
   return (
-    <div
-      className="result-page"
-      style={{ visibility: showResult ? "visible" : "hidden" }}
-    >
-      <h2>Your results will be here</h2>
-      <div>{quote[0]}</div>
-      <div>{quote[1]}</div>
+    <div className="result-page" style={{ visibility: showResult ? "visible" : "hidden" }}>
+      <p className="quote text-glow">{quote && quote[0]}</p>
+      <p className="quote text-glow">{quote && quote[1]}</p>
       <div>
-        <h3>Your reccomendations are</h3>
-        <div>{data.recommendations}</div>
+        {loading && <img className="loading-icon" src="./loading.svg" />}
+        {!loading && !error && data && (
+          <div>
+            <br />
+            <h2>Your movie match is</h2>
+            <h1 class="your-match text-glow">{!data.data ? '¯\\_(ツ)_/¯' : data.data.match}</h1>
+            {data.data.recommendations && <p className="ai-recommended-title">AI recommended movies you may like</p>}
+            {data.data.recommendations && <p className="ai-recommended-content text-glow">{data.data.recommendations}</p>}
+          </div>
+        )}
+        {!loading && !error && !data && <div>No data available</div>}
+        {!loading && <button className="button" onClick={() => window.location.reload()}>Restart</button>}
       </div>
     </div>
   );
